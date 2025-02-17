a65d4604f9d3bfa9c6c1eb0b55c4d8f6261081e2
vmath20 <vaibhavrichmishra@gmail.com>
2025-02-17 15:26:59 +0530

    Your descriptive commit message

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,13 @@
- 
+import { clerkMiddleware } from '@clerk/nextjs/server'
+
+export default clerkMiddleware()
+
+// See https://clerk.com/docs/references/nextjs/auth-middleware
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
