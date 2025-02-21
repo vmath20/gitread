96bdc71a29a414a031fa0e5d10c026e7c67689e3
vmath20 <vaibhavrichmishra@gmail.com>
2025-02-21 14:43:33 +0530

    Update Stripe API version

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -3,7 +3,7 @@ import { getAuth } from '@clerk/nextjs/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-01-27.acacia',
 });
 
 export async function POST(req: Request) {
